2586e90d5ed111ee15d62b6a56320087d9b18228
Zack Weathersby <zweathersby@Zacks-MBP-2.attlocal.net>
2024-09-04 12:46:56 -0400

    transitions and icons

--- app/ui/sections/contact.tsx
+++ app/ui/sections/contact.tsx
@@ -4,7 +4,9 @@ import Subtitle from "@/app/ui/components/subtitle";
 import ContactForm from "@/app/ui/components/contact-form";
 import FadeInWhenVisible from "@/app/ui/fade-in";
 
-import "../contact.module.css";
+import styles from "@/app/ui/contact.module.css";
+
+const { phoneIcon } = styles;
 
 export default function Contact() {
   return (
@@ -20,23 +22,25 @@ export default function Contact() {
                 Don&apos;t hesitate to email or call us if you&apos;d like to
                 schedule a tour or need more information!
               </p>
-              <Subtitle className="text-3xl font-normal flex items-center gap-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  strokeWidth={0.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
-                  />
-                </svg>
-                (843) 559-3938
-              </Subtitle>
+              <a href={`tel:${8435593938}`} className={phoneIcon}>
+                <Subtitle className="text-3xl font-normal flex items-center gap-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    strokeWidth={0.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                    />
+                  </svg>
+                  (843) 559-3938
+                </Subtitle>
+              </a>
             </div>
 
             {/* Email Contact Form */}
